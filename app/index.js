59157607b77b983c025e5e6d492a4b7e8434cb56
internetdrew <andrew@internetdrew.com>
2023-07-04 13:29:28 -0400

    add supporting starter files

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Home, my g!</Text>
     </View>
   );
 };
