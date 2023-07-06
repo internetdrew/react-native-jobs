40fa87709b69ee346a0f2c9383a61720402a45ee
internetdrew <andrew@internetdrew.com>
2023-07-06 12:25:49 -0400

    render out job tabs for dynamic components

---
+++ app/job-details/[id].js
@@ -0,0 +1,117 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hooks/useFetch';
+
+const tabs = ['About', 'Qualifications', 'Responsibilities'];
+
+const JobDetails = () => {
+  const { id: jobId } = useLocalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: jobId,
+  });
+
+  const jobDetails = data?.[0];
+
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const onRefresh = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case 'Qualifications':
+        return (
+          <Specifics
+            title='Qualifications'
+            points={jobDetails?.job_highlights?.qualifications ?? ['N/A']}
+          />
+        );
+      case 'About':
+      case 'Responsibilities':
+      default:
+        break;
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension='60%'
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension='60%'
+              // handlePress={() => router.back()}
+            />
+          ),
+          headerTitle: '',
+        }}
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size={'large'} color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data matching</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={jobDetails?.employer_logo}
+                jobTitle={jobDetails?.job_title}
+                companyName={jobDetails?.employer_name}
+                location={jobDetails?.job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
