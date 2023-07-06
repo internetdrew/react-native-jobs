750068ed4d406b679e4d228fa460a92e3c038a9c
internetdrew <andrew@internetdrew.com>
2023-07-06 10:53:46 -0400

    improve function param naming

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { RAPID_API_KEY } from '@env';
 
-const useFetch = (endpoint, query) => {
+const useFetch = (endpoint, params) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
   const options = {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    params: { ...query },
+    params: { ...params },
     headers: {
       'X-RapidAPI-Key': RAPID_API_KEY,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
